0834414ca449708599907700c06175c41779b8c3
Guillermo Bescos <gbescos@stanford.edu>
2020-05-15 02:05:20 -0700

    rename sorted

--- rename.py
+++ rename.py
@@ -2,7 +2,7 @@ import os
 os.chdir('data')
 for directory in ['Cover','JMiPOD','JUNIWARD','UERD','Test']:
     os.chdir(directory)
-    L_image=os.listdir()
+    L_image=sorted(os.listdir())
     i=0
     for i in range(len(L_image)):
         os.rename(L_image[i],str(i).rjust(5,"0")+".jpg")
