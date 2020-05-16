ff187c7faf489479c1f29e46fb2453a8b6c752af
Guillermo Bescos <gbescos@stanford.edu>
2020-05-16 01:42:17 -0700

    commented importnet

--- models.py
+++ models.py
@@ -6,19 +6,19 @@ from layers import Layer1,Layer2,Layer3,Layer4
 
 
 #pip install efficientnet-pytorch
-from efficientnet_pytorch import EfficientNet
+# from efficientnet_pytorch import EfficientNet
 
-class Net(nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
-        self.model = EfficientNet.from_pretrained('efficientnet-b0')
-        # 1280 is the number of neurons in last layer. is diff for diff. architecture
-        self.dense_output = nn.Linear(1280, num_classes)
+# class Net(nn.Module):
+#     def __init__(self, num_classes):
+#         super().__init__()
+#         self.model = EfficientNet.from_pretrained('efficientnet-b0')
+#         # 1280 is the number of neurons in last layer. is diff for diff. architecture
+#         self.dense_output = nn.Linear(1280, num_classes)
 
-    def forward(self, x):
-        feat = self.model.extract_features(x)
-        feat = F.avg_pool2d(feat, feat.size()[2:]).reshape(-1, 1280)
-        return self.dense_output(feat)
+#     def forward(self, x):
+#         feat = self.model.extract_features(x)
+#         feat = F.avg_pool2d(feat, feat.size()[2:]).reshape(-1, 1280)
+#         return self.dense_output(feat)
         
 
 class SRNET(nn.Module):
