db2526db2300c6fb0dc38c5c0887af7a1a802c9e
axmoyal <axmoyal@stanford.edu>
2020-05-21 15:19:47 -0700

    model clean

--- models.py
+++ models.py
@@ -7,17 +7,6 @@ import torchvision.models as models
 
 # from efficientnet_pytorch import EfficientNet
 
-# class EfficientNet(nn.Module):
-#     def __init__(self, num_classes):
-#         super().__init__()
-#         self.model = EfficientNet.from_pretrained('efficientnet-b0')
-#         # 1280 is the number of neurons in last layer. is diff for diff. architecture
-#         self.dense_output = nn.Linear(1280, num_classes)
-
-#     def forward(self, x):
-#         feat = self.model.extract_features(x)
-#         feat = F.avg_pool2d(feat, feat.size()[2:]).reshape(-1, 1280)
-#         return self.dense_output(feat)
 
 
 class ResNet(nn.Module):
